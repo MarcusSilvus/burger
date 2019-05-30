a3d04c0d8573c1288997e472fa4ad369d9668e92
Mark Silva <marksilva@D-10-19-58-249.dhcp4.washington.edu>
2019-05-29 17:39:55 -0700

    db schema and seeds added

---
+++ db/schema.sql
@@ -0,0 +1,12 @@
+DROP DATABASE IF EXISTS burgers_db;
+
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers (
+  id INT NOT NULL AUTO_INCREMENT,
+  burger_name VARCHAR(45) NOT NULL,
+  devoured BOOLEAN NOT NULL DEFAULT 0,
+  PRIMARY KEY (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name)
+VALUES ("classic burger");
+
+INSERT INTO burgers (burger_name)
+VALUES ("cheese burger");
+
+INSERT INTO burgers (burger_name)
+VALUES ("bacon burger");
