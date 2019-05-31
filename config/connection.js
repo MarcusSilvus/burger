b1fa4b52b3c8eb5ba383cc11c4dc409ad1c8638a
Mark Silva <marksilva@Marks-MacBook-Pro.local>
2019-05-30 20:02:06 -0700

    updated server.js

--- config/connection.js
+++ config/connection.js
@@ -1,2 +1,16 @@
 var mysql = require("mysql");
 require("dotenv").config();
+
+
+var connection = mysql.createConnection({
+  host: 'localhost',
+  port: '3306',
+  user: 'root',
+  password: sqlpass,
+  database: 'burgers_db'
+});
+
+connection.connect(function (err) {
+  if (err) throw err;
+  openShop();
+});
