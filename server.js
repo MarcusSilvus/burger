011d6159d9cc3da42fc1114ff1f48ca52aa104fa
Mark Silva <mark.allen.silva@gmail.com>
2019-06-02 10:52:16 -0700

    got css and js path working with middleware

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var PORT = process.env.PORT || 8080;
 var app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static("public"));
+app.use(express.static('public'));
 
 // Parse application body
 app.use(bodyParser.urlencoded({ extended: false }));
