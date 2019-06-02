461380e49ea32eafa783b0a9d2442cdfc3cdc105
Mark Silva <mark.allen.silva@gmail.com>
2019-06-01 22:49:55 -0700

    playing with addJS logic

---
+++ public/js/app.js
@@ -0,0 +1,44 @@
+var burgerTemplate = (burgerName, id, id_dovoured) => {
+
+  var burgerContainer = $('<div>').attr({
+    class: 'content-burger__list',
+    id: id
+  });
+
+  var img = $('<img>').attr({
+    src: '',
+    alt: ''
+  });
+
+  var name = $('<p>');
+
+  var button = $('<button>').attr({
+    'data-id': id,
+    class: 'btn btn-primary devoured',
+    'data-state': is_devoured
+  });
+
+  name.html(burgerName)
+
+};
+
+
+$('button[type="submit"]').on('click', function (event) {
+  event.preventDefault();
+
+  var burgerName = $('input[name="burger_name"]').val();
+
+  $.ajax({
+    url: '/add',
+    method: 'POST',
+    data: {
+      burger_name: burgerName
+    }
+  })
+    .then(function () {
+      alert('burger added')
+    })
+    .catch(function () {
+      alert('burger not added')
+    });
+});
