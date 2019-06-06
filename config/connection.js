ac533a8c0ef30ae7c712e997ac1cbd4d5b03dea3
Mark Silva <mark.allen.silva@gmail.com>
2019-06-05 17:50:52 -0700

    setting up remote db

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,19 @@
 var mysql = require("mysql");
 //require("dotenv").config();
+var connection;
 
-
-var connection = mysql.createConnection({
-  host: 'localhost',
-  port: '3306',
-  user: 'root',
-  password: 'squackBox1!',
-  database: 'burgers_db'
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    port: '3306',
+    user: 'root',
+    password: 'squackBox1!',
+    database: 'burgers_db'
+  });
+};
 
 connection.connect(function (err) {
   if (err) throw err;
@@ -16,3 +21,5 @@ connection.connect(function (err) {
 });
 
 module.exports = connection;
+
+g8mh6ge01lu2z3n1.cbetxkdyhwsb.us - east - 1.rds.amazonaws.com
